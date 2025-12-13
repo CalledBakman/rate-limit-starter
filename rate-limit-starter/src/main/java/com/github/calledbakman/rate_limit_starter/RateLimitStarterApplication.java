68d987cd2471618343e952251e4a47a13e2559dd
CalledBakman <bakmandev@gmail.com>
2025-12-13 22:15:01 +0330

    initial commit

---
+++ rate-limit-starter/src/main/java/com/github/calledbakman/rate_limit_starter/RateLimitStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.calledbakman.rate_limit_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimitStarterApplication.class, args);
+	}
+
+}
---
+++ rate-limit-starter/src/test/java/com/github/calledbakman/rate_limit_starter/RateLimitStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.calledbakman.rate_limit_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimitStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
