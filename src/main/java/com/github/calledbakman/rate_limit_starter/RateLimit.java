2b28b982c3d2aadde2917e271ba978e998dd2430
CalledBakman <bakmandev@gmail.com>
2026-06-26 00:17:10 +0330

    - better documenting - changing cleanUpTtl to cleanUpInterval for better reading and change default value to milliseconds instead of minutes

--- src/main/java/com/github/calledbakman/rate_limit_starter/RateLimit.java
+++ src/main/java/com/github/calledbakman/rate_limit_starter/RateLimit.java
@@ -2,7 +2,6 @@ package com.github.calledbakman.rate_limit_starter;
 
 import com.github.calledbakman.rate_limit_starter.enums.KeyType;
 import com.github.calledbakman.rate_limit_starter.enums.PeriodType;
-import com.github.calledbakman.rate_limit_starter.enums.RateLimitScope;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,24 +11,36 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface RateLimit {
-    //This key will be used to decide client-base or method-base approach to limit, you can ignore it to be method base
+    /**
+     * This key will be used to decide client-base or method-base approach to limit. You can ignore it to be method base
+     */
     KeyType key() default KeyType.METHOD_SIGN;
 
-    //The limit you set that client can send to an endpoint
+    /**
+     * The limit you set that client can send to an endpoint
+     */
     int limit() default 5;
 
-    //Period of the limitation
+    /**
+     * Period of the limitation
+     */
     PeriodType period() default PeriodType.MIN;
 
-    //(Optional) Duration you set for period the defaults are : 1000 millis for SEC (1 Second), 60000 millis for MIN (1 Minute), 3600000 millis for HOUR (1 Hour)
+    /**
+     * (Optional) Duration you set for the period
+     * The defaults are:
+     * 1000 millis for SEC (1 Second),
+     * 60,000 millis for MIN (1 Minute),
+     * 3,600,000 millis for HOUR (1 Hour)
+     */
     int duration() default 1;
 
     /**
      * Cleanup Time-To-Live in minutes
      * -1 = Cleanup immediately after rate limit window passes
      * 0 = Never cleanup (use with caution)
-     * >0 = Cleanup after X minutes of inactivity
+     * >0 = Cleanup after X milliseconds of inactivity
      * Default: 60 (1 hour of inactivity)
      */
-    int cleanUpTtl() default 60;
+    int cleanUpInterval() default 60000;
 }
